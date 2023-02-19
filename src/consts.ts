b41e3d2f7bf9f09561d86b48269e6cf9bcd548f7
pontakornth <pontakorn61@gmail.com>
2023-02-19 21:59:21 +0700

    more accurate description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Pontakorn's Blog";
-export const SITE_DESCRIPTION = "Development, Horror, and more";
+export const SITE_DESCRIPTION =
+  "I write whatever I like: development, story, and probably more.";
