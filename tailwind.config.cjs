7c316652168d3a6715ee7422978dab6cfbf7a403
pontakornth <pontakorn61@gmail.com>
2023-02-21 15:57:48 +0700

    Use fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["IBM Plex Sans Thai Looped", ...defaultTheme.fontFamily.sans],
+        heading: ["IBM Plex Sans Thai", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
